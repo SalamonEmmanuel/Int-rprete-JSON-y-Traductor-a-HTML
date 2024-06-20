b12fc7c47d8e12a3fa2cf624e740f22768cdec8c
SalamonEmmanuel <148009083+SalamonEmmanuel@users.noreply.github.com>
2024-06-20 16:42:36 -0300

    Update main.py
    Anteriormente, las expresiones regulares para integer, float, bool y date usaban anclajes ^ y $, lo cual obligaba a que coincidieran con toda la línea. Esto causaba problemas cuando los valores eran seguidos inmediatamente por una coma o cualquier otro carácter.
    
    Solución:
    ○ Se eliminaron estos anclajes para permitir que las expresiones coincidan con los patrones de los datos sin importar los caracteres que los sigan.
    
    Otros cambios:
    
    Debemos asegurarnos de que la expresión regular de float se verifique antes que la de integer, para que cuando venga un real (por ejemplo, 288,5), el lexer no lo separe en un "t_entero t_punto t_entero". 
    
    La función t_float ahora se define antes de t_integer. Esto asegura que los números decimales se reconozcan correctamente antes que los enteros.

--- main.py
+++ main.py
@@ -1,14 +1,12 @@
 import ply.lex as lex
-import re
 
 # Definición de los tokens
 tokens = ['string', 'integer', 'float', 'bool', 'date', 't_version', 't_firma_digital',
-        't_nombre_empresa', 't_fundacion', 't_calle', 't_ciudad', 't_pais',
-        't_ingresos_anuales', 't_pyme', 't_link', 't_nombre', 't_jefe', 
-        't_edad', 't_cargo', 't_salario', 't_activo','t_fecha_contratacion', 
-        't_estado', 't_fecha_inicio', 't_fecha_fin', 'CARGO_EMPLEADO', 'coma', 
-        'pa', 'dp', 'pc', 'lla', 'llc', 'patron_url', 'ESTADO_PROYECTO']
-
+          't_nombre_empresa', 't_fundacion', 't_calle', 't_ciudad', 't_pais',
+          't_ingresos_anuales', 't_pyme', 't_link', 't_nombre', 't_jefe', 
+          't_edad', 't_cargo', 't_salario', 't_activo', 't_fecha_contratacion', 
+          't_estado', 't_fecha_inicio', 't_fecha_fin', 'CARGO_EMPLEADO', 'coma', 
+          'pa', 'dp', 'pc', 'lla', 'llc', 'patron_url', 'ESTADO_PROYECTO']
 
 reservadas = {
     '"version"': 't_version',
@@ -34,24 +32,20 @@ reservadas = {
 }
 
 # Definición de las expresiones regulares para los tokens
-
-#la primera barra invertida hace que los metacaracteres del paréntesis y las llaves se interpreten literalmente y no como su significado especial en expresiones regulares.
-# necesitamos colocar una barra invertida adicional para indicar que la primera barra invertida debe interpretarse literalmente como '\', ya que también es un metacaracter
-t_pa = '\\['
-t_dp = ':'
-t_pc = '\\]'
-t_lla = '\\{'
-t_llc = '\\}'
-t_coma = ','
+t_pa = r'\['
+t_dp = r':'
+t_pc = r'\]'
+t_lla = r'\{'
+t_llc = r'\}'
+t_coma = r','
 
 # Ignorar espacios en blanco y tabulaciones
 t_ignore = ' \t'
 
 def t_patron_url(t):
-    r'(https?://[a-zA-Z0-9.-]+(?:\:\d+)?(?:/[a-zA-Z0-9._/#-]*)?)'
-    t.value = f"patron_url: {t.value}"  
+    r'https?://[a-zA-Z0-9.-]+(?:\:\d+)?(?:/[a-zA-Z0-9._/#-]*)?'
+    t.value = f"patron_url: {t.value}"
     return t
-
 
 def t_CARGO_EMPLEADO(t):
     r'"(?:Product\sAnalyst|Project\sManager|UX\sdesigner|Marketing|Developer|Devops|DB\sadmin)"'
@@ -63,12 +57,23 @@ def t_ESTADO_PROYECTO(t):
     t.value = f"ESTADO_PROYECTO: {t.value[1:-1]}"  # Excluimos las comillas dobles
     return t
 
-
-#Al colocar la función t_date antes de la función t_string, el lexer primero intentará reconocer la entrada como una fecha antes de intentar reconocerla como una cadena de caracteres.
-def t_date(t):
-    r'^"(19[0-9]{2}|20[0-9]{2})-(0[1-9]|1[0-2])-(0[1-9]|[12][0-9]|3[01])"$'
+def t_float(t):
+    r'\d+\.\d+'
+    t.value = float(t.value)
     return t
 
+def t_integer(t):
+    r'\d+'
+    t.value = int(t.value)
+    return t
+
+def t_bool(t):
+    r'true|false'
+    return t
+
+def t_date(t):
+    r'"(19[0-9]{2}|20[0-9]{2})-(0[1-9]|1[0-2])-(0[1-9]|[12][0-9]|3[01])"'
+    return t
 
 def t_string(t):
     r'"(?:[^"\\]|\\.)*"'
@@ -78,35 +83,13 @@ def t_string(t):
         t.type = 'string'
     return t
 
-
-def t_integer(t):
-    r'^\d+$'
-    t.value = int(t.value)
-    return t
-
-
-def t_float(t):
-    r'^\d+\.\d{1,2}$'
-    t.value = float(t.value)
-    return t
-
-
-def t_bool(t):
-    r'^(true|false)$'
-    return t
-
-
 def t_newline(t):
     r'\n+'
     t.lexer.lineno += len(t.value)
 
-
 def t_error(t):
     print("Caracter no reconocido: '%s'" % t.value[0])
     t.lexer.skip(1)
-
-
-
 
 lexer = lex.lex()
 
@@ -114,11 +97,8 @@ print("Ingrese los tokens (ingrese 'finalizar' para terminar):")
 
 while True:
     entrada = input("> ")
-    lexer.input(entrada)
     if entrada == 'finalizar':
         break
-   
+    lexer.input(entrada)
     for token in lexer:
         print(f"Token: {token.type}, Valor: {token.value}")
-
-
